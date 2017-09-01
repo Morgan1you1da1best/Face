706b0a7b55bb3349bbc919a2a03990bfad280774
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-09-01 14:43:01 -0400

    Updated from Brython Server: 9/1/2017 2:43:00 PM

--- face.py
+++ face.py
@@ -1,1 +1,10 @@
+#Morgan Baughman
+#9/1/17
+#face.py
 
+print( '      XXXXX' )
+print( '   X         X' )                            
+print( ' ((    O O    ))' )                            
+print( '    |   V   |')
+print( '    | ===== |')
+print( '     ------- ') 
